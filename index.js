d4218b1f9ee75f26d80c5fbe7f61c4ac440a9f74
SniffKim <103081948+SniffKim@users.noreply.github.com>
2022-04-06 20:33:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-// 1. Create global userWalletAddress variable
-window.userWalletAddress = null;
+// 1. Web3 login function
+const loginWithEth = async () => {
+  // 1.1 check if there is global window.web3 instance
+  if (window.web3) {
+    try {
+      // 2. get the user's ethereum account - prompts metamask to login
+      const selectedAccount = await window.ethereum
+        .request({
+          method: "eth_requestAccounts",
+        })
+        .then((accounts) => accounts[0])
+        .catch(() => {
+          // 2.1 if the user cancels the login prompt
+          throw Error("Please select an account");
+        });
 
-// 2. when the browser is ready
-window.onload = async (event) => {
+      // 3. set the global userWalletAddress variable to selected account
+      window.userWalletAddress = selectedAccount;
 
-  // 2.1 check if ethereum extension is installed
-  if (window.ethereum) {
+      // 4. store the user's wallet address in local storage
+      window.localStorage.setItem("userWalletAddress", selectedAccount);
 
-    // 3. create web3 instance
-    window.web3 = new Web3(window.ethereum);
+      // 5. show the user dashboard
+      showUserDashboard();
 
+    } catch (error) {
+      alert(error);
+    }
   } else {
-
-    // 4. prompt user to install Metamask
-    alert("Please install MetaMask or any Ethereum Extension Wallet");
+    alert("wallet not found");
   }
-
-  // 5. check if user is already logged in and update the global userWalletAddress variable
-  window.userWalletAddress = window.localStorage.getItem("userWalletAddress");
-
-  // 6. show the user dashboard
-  showUserDashboard();
 };
 
+// 6. when the user clicks the login button run the loginWithEth function
+document.querySelector(".login-btn").addEventListener("click", loginWithEth);
